e5a8cbcd7284b74ccf9d4776269e27cdd0ae2138
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-02-14 14:42:28 +0100

    add key tag

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
 
     [SerializeField] private GameObject key;
-    [SerializeField] private GameObject player;
+    [SerializeField] private Inventory playerInventory;
 
     public void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player"))
+        {
+            if (playerInventory.hasItem(key))
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
