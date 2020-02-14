d54b24bc38021afee3fd1db9a11c02a2131934af
Marco Beijen <mbeijen1@gmail.com>
2020-02-14 14:15:04 +0100

    add second floor

---
+++ Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 5f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(hor, 0, ver);
+        
+        rb.AddForce(direction * (speed * Time.deltaTime));
+    }
+}
